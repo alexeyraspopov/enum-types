94c03ee415cf982e933a78863563efc5aa89ab38
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-05-25 14:25:43 +0300

    s/Enum/Mask and correct Enum implementation (WIP)

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-function Enum(keys, hasSum) {
+function Mask(keys, hasSum) {
 	var acc = hasSum ? { ALL: Math.pow(2, keys.length) - 1 } : {};
 
 	return Object.freeze(keys.reduce(function(acc, key, index) {
 		acc[key] = Math.pow(2, index);
 		return acc;
 	}, acc));
+}
+
+function Enum(object) {
+	var keys = Object.keys(object),
+		values = keys.reduce(function(array, key, index) {
+			array.push(object[key] == null ? (index > 0 ? array[index - 1] + 1 : 0) : object[key]);
+            return array;
+		}, []);
+
+	return Object.freeze(keys.reduce(function(acc, key, index) {
+		var value = values[index];
+
+		acc[acc[key] = value] = key;
+
+		return acc;
+	}, {}));
 }
 
 function Mirror(list) {
@@ -16,5 +32,6 @@ function Mirror(list) {
 	}, {}));
 }
 
+exports.Mask = Mask;
 exports.Enum = Enum;
 exports.Mirror = Mirror;
